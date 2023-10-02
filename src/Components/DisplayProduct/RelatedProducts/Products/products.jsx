e674a0bbc5fd01a9506a2ea90bf3edb418c789b9
Aminur-Rahman-Kanon <kanon754@gmail.com>
2023-10-02 03:57:54 +0100

    product listing 1

--- src/Components/DisplayProduct/RelatedProducts/Products/products.jsx
+++ src/Components/DisplayProduct/RelatedProducts/Products/products.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import styles from './products.module.css';
+import { Link } from 'react-router-dom';
 
 const Products = ({ product }) => {
     return (
-        <div className={styles.products}>
+        <Link to={`/bag/${product.category}/${product.title}`} className={styles.products}>
             <div className={styles.imgContainer}>
                 <img src={Object.values(product.img)[0]} alt={product.title} className={styles.img}/>
             </div>
@@ -11,7 +12,7 @@ const Products = ({ product }) => {
                 <span className={styles.title}>{product.title}</span>
                 <span className={styles.price}>&#2547;{product.price.originalPrice}</span>
             </div>
-        </div>
+        </Link>
     )
 }
 
