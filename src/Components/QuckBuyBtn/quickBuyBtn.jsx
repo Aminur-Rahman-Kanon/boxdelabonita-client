8d12f191133de64078760f90122c4bc083d95862
Aminur-Rahman-Kanon <kanon754@gmail.com>
2023-12-25 03:40:41 +0000

    login implementaion

--- src/Components/QuckBuyBtn/quickBuyBtn.jsx
+++ src/Components/QuckBuyBtn/quickBuyBtn.jsx
@@ -3,6 +3,7 @@ import styles from './quickBuyBtn.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { toast } from 'react-toastify';
+import { addToCart } from '../../Utilities/utilities';
 
 const QuickBuyBtn = ({ product, color }) => {
 
@@ -18,31 +19,20 @@ const QuickBuyBtn = ({ product, color }) => {
 
         if (!color){
             setIsLoading(false);
-            return toast.info('Please Select a Color', {style: {backgroundColor: '#067FD0', textTransform: 'capitalize'}});
+            return toast.info('Please Select a Color', {style: {backgroundColor: '#067FD0', textTransform: 'capitalize', fontWeight: '600'}});
         }
 
-        await fetch('https://boxdelabonita-server-13dd.onrender.com/add-item', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ product, color })
-        }).then(res => res.json())
-        .then(result => {
-            if (result.status === 'success'){
-                setIsLoading(false);
-                toast.success(`${product.title} added to cart`, {style: {backgroundColor: '#4b7d37', textTransform: 'capitalize'}})
-                return window.location.assign('/checkout');
-            }
-            else {
-                setIsLoading(false);
-                toast.success(`${result.status}`);
-            }
-        })
-        .catch(err => {
+        const status = addToCart(product, color);
+        
+        if (status === 'success'){
             setIsLoading(false);
-            toast.error('Failed', {style: {textTransform: 'capitalize'}});
-        });
+            toast.success(`${product.title} added to cart`, {style: {backgroundColor: '#4b7d37', textTransform: 'capitalize', fontWeight: '600'}})
+            return window.location.assign('/checkout');
+        }
+        else {
+            setIsLoading(false);
+            toast.error('Failed', {style: {textTransform: 'capitalize', fontWeight: '600'}});
+        }
     }
 
     return (
